2a1fea98b852f629f9b8907b3c8ff64f00aff9a2
Marlon0714 <pulgita-0714@hotmail.com>
2024-04-04 22:51:13 -0500

    Prueba Lab2.3

---
+++ src/main/java/co/edu/uniquindio/controllers/HomeController.java
@@ -0,0 +1,22 @@
+// ClienteController.java
+package co.edu.uniquindio.controllers;
+
+import co.edu.uniquindio.model.dto.ClienteDTO;
+import co.edu.uniquindio.services.interfaces.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public ResponseEntity<Map<String,String>> healthcheck() {
+        return ResponseEntity.ok().body(Map.of("Status","Up"));
+    }
+
+}
